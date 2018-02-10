cd3ebbfd6a41a6f66adf599d870041dc9dc7c926
romiogaku <romiogaku@me.com>
2018-02-10 17:53:13 +0900

    テーブルビューサンプル

--- design/resources/aikatsu/resource.go
+++ design/resources/aikatsu/resource.go
@@ -15,6 +15,14 @@ var _ = Resource("Aikatsu", func() {
 			Media(OjisanMT)
 		})
 	})
+	Action("GetAikatsuUsers", func() {
+		Routing(GET("/users"))
+		Description("ユーザ一覧を返します")
+		Response(OK, func() {
+			Description("正常に取得できた場合に返却")
+			Media(CollectionOf(UserMT))
+		})
+	})
 })
 
 // OjisanMT おじさんMediaType
@@ -26,5 +34,19 @@ var OjisanMT = MediaType("application/vnd.romiogaku.com.goaviron.aikatsu.ojisan+
 	})
 	View("default", func() {
 		Attribute("value")
+	})
+})
+
+// UserMT ユーザMediaType
+var UserMT = MediaType("application/vnd.romiogaku.com.goaviron.aikatsu.user+json", func() {
+	ContentType("application/vnd.romiogaku.com.goaviron.aikatsu.user+json; charset=utf8")
+	Attributes(func() {
+		Attribute("id", Integer)
+		Attribute("name", String)
+		Required("id", "name")
+	})
+	View("default", func() {
+		Attribute("id")
+		Attribute("name")
 	})
 })
--- presentation/controller/aikatsu.go
+++ presentation/controller/aikatsu.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"goaviron/app"
 	"math/rand"
 	"time"
@@ -16,6 +17,26 @@ type AikatsuController struct {
 // NewAikatsuController creates a Aikatsu controller.
 func NewAikatsuController(service *goa.Service) *AikatsuController {
 	return &AikatsuController{Controller: service.NewController("AikatsuController")}
+}
+
+// GetAikatsuUsers runs the GetAikatsuUsers action.
+func (c *AikatsuController) GetAikatsuUsers(ctx *app.GetAikatsuUsersAikatsuContext) error {
+	// AikatsuController_GetAikatsuUsers: start_implement
+
+	// Put your logic here
+
+	res := make(app.RomiogakuComGoavironAikatsuUserCollection, 0)
+	sum := 0
+	for i := 1; i <= 10; i++ {
+		sum += i
+		u := &app.RomiogakuComGoavironAikatsuUser{
+			ID:   i,
+			Name: fmt.Sprintf("user%d", i),
+		}
+		res = append(res, u)
+	}
+	return ctx.OK(res)
+	// AikatsuController_GetAikatsuUsers: end_implement
 }
 
 // GetOjisanNum runs the GetOjisanNum action.
